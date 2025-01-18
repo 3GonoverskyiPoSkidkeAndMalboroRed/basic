f06f8b5b389274fca5efa18a3a6d9afff5d7ed80
Pavel Neverov <140485683+3GonoverskyiPoSkidkeAndMalboroRed@users.noreply.github.com>
2025-01-18 14:55:18 +0300

    добавили маску +7(999)-999-99-99 на ввож телефона

--- views/site/register.php
+++ views/site/register.php
@@ -14,9 +14,9 @@ use yii\bootstrap5\ActiveForm;
     <?php $form = ActiveForm::begin(); ?>
 
         <?= $form->field($model, 'login') ?>
-        <?= $form->field($model, 'password') ?>
+        <?= $form->field($model, 'password')->passwordInput() ?>
         <?= $form->field($model, 'full_name') ?>
-        <?= $form->field($model, 'phone') ?>
+        <?= $form->field($model, 'phone')-> widget(\yii\widgets\MaskedInput::class,['mask'=> '+7(999)-999-99-99', ]) ?>
         <?= $form->field($model, 'email') ?>
     
         <div class="form-group">
