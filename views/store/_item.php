251569b56516fbc1d345e5c02c8dc268d6de09da
Pavel Neverov <140485683+3GonoverskyiPoSkidkeAndMalboroRed@users.noreply.github.com>
2025-02-23 12:39:02 +0300

    Добавлена рамка у товара в карточке товара и каталоге, убрана кнопка переделан в блок-ссылку

--- views/store/_item.php
+++ views/store/_item.php
@@ -1,19 +1,19 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 /** @var app\models\Product $model */
 
 ?>
-<div class="item">
-    <h3><?= Html::encode($model->title) ?></h3>
-    <p>Цена: <?= Html::encode($model->cost) ?> руб.</p>
-    
-    <?php if ($model->photos): ?>
-        <div class="product-image">
-            <?= Html::img('@web/uploads/' . $model->photos[0]->file_name, ['alt' => $model->title, 'class' => 'img-thumbnail', 'width' => '319.5', 'height' => '319.5']) ?>
-        </div>
-    <?php endif; ?>
-    
-    <p><?= Html::a('Просмотреть', ['view', 'id' => $model->id], ['class' => 'btn btn-primary']) ?></p>
+<div class="item" style="width: 319.5px; border: 1px solid #ccc; padding: 10px; border-radius: 5px; overflow: hidden;">
+    <a href="<?= Url::to(['view', 'id' => $model->id]) ?>" style="text-decoration: none; color: inherit;">
+        <?php if ($model->photos): ?>
+            <div class="product-image">
+                <?= Html::img('@web/uploads/' . $model->photos[0]->file_name, ['alt' => $model->title, 'class' => 'img-thumbnail', 'style' => 'border: 1px solid #ccc; border-radius: 0; width: 100%; height: auto;']) ?>
+            </div>
+        <?php endif; ?>
+        <h3><?= Html::encode($model->title) ?></h3>
+        <p>Цена: <?= Html::encode($model->cost) ?> руб.</p>
+    </a>
 </div> 
--- views/store/view.php
+++ views/store/view.php
@@ -9,16 +9,14 @@ $this->title = $model->title;
 $this->params['breadcrumbs'][] = ['label' => 'Каталог товаров', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="product-view">
-
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="product-view" style="border: 1px solid #ccc; width: 300px; padding: 10px; border-radius: 5px; overflow: hidden;">
 
     <?php if ($model->photos): ?>
         <div class="product-image">
-            <?= Html::img('@web/uploads/' . $model->photos[0]->file_name, ['alt' => $model->title, 'class' => 'img-thumbnail', 'width' => '300', 'height' => '300']) ?>
+            <?= Html::img('@web/uploads/' . $model->photos[0]->file_name, ['alt' => $model->title, 'class' => 'img-thumbnail', 'style' => 'border: 1px solid #ccc; border-radius: 0; width: 100%; height: auto;']) ?>
         </div>
     <?php endif; ?>
-
+    <h2><?= Html::encode($this->title) ?></h2>
     <p>Цена: <?= Html::encode($model->cost) ?> руб.</p>
     <p>Количество: <?= Html::encode($model->count) ?></p>
     <p>Категория: <?= Html::encode($model->category->title) ?></p>
