399cc30fffdf9bfe26c1c4253513805de37b294a
Pavel Neverov <140485683+3GonoverskyiPoSkidkeAndMalboroRed@users.noreply.github.com>
2025-05-01 15:28:44 +0300

    стили для страница Музыка

--- views/music/_item.php
+++ views/music/_item.php
@@ -4,15 +4,14 @@ use yii\helpers\Html;
 /** @var app\models\Music $model */
 ?>
 
-
-    <div class="youtube-thumbnail">
+    <div class="card" style="width: 18rem; background-color: #000; color: #fff;">
         <?php if ($model->getYoutubeThumbnail()): ?>
-            <img src="<?= $model->getYoutubeThumbnail() ?>" 
-                 alt="<?= Html::encode($model->title) ?>"
-            >
-            <p class="music-title-text">
-                <?= Html::encode($model->title) ?>
-            </p>
+            <a href="<?= $model->youtube_link ?>" class="youtube-thumbnail" style="text-decoration: none; color: white; font-family: Impact;">
+                <img src="<?= $model->getYoutubeThumbnail() ?>" class="card-img-top" alt="<?= Html::encode($model->title) ?>">
+                <div class="card-body">
+                    <p class="card-text" style="font-family: Impact;"><?= Html::encode($model->title) ?></p>
+                </div>
+            </a>
         <?php else: ?>
             <div class="no-image">
                 Превью недоступно
@@ -32,4 +31,3 @@ use yii\helpers\Html;
             ]) ?>
         </div>
     <?php endif; ?>
-<?= Html::endTag('a') ?>
