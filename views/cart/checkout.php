c560713126b1aeb1e05f19d2e78bb21612c5920f
Pavel Neverov <140485683+3GonoverskyiPoSkidkeAndMalboroRed@users.noreply.github.com>
2025-05-04 17:06:01 +0300

    добавлена мавска ввода и стили

--- views/cart/checkout.php
+++ views/cart/checkout.php
@@ -2,20 +2,39 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\widgets\MaskedInput;
 
 /** @var yii\web\View $this */
 /** @var yii\base\DynamicModel $model */
 
 $this->title = 'Оформление заказа';
 ?>
+<style>
+    h1 {
+        font-family: 'Impact', sans-serif; /* Устанавливаем шрифт Impact для заголовка */
+        color: white; /* Цвет заголовка */
+        text-align: center; /* Центрирование заголовка */
+        margin-top: 1rem; /* Отступ сверху */
+    }
+    body {
+        font-family: 'Helvetica', sans-serif; /* Устанавливаем шрифт Helvetica для всего тела страницы */
+    }
+</style>
+
 <h1><?= Html::encode($this->title) ?></h1>
 
 <div class="order-checkout">
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'contact_number')->textInput(['maxlength' => true, 'placeholder' => 'Введите номер телефона']) ?>
+    <?= $form->field($model, 'contact_number')->widget(MaskedInput::class, [
+        'mask' => '+7(999)-999-99-99',
+        'options' => [
+            'placeholder' => 'Введите номер телефона',
+            'maxlength' => true,
+        ],
+    ])->label('Номер телефона') ?>
 
-    <div class="form-group">
+    <div class="form-group" style="margin-top: 0.3rem;">
         <?= Html::submitButton('Подтвердить', ['class' => 'btn btn-success']) ?>
     </div>
 
