35f16bf7d2e2305800f3d42bd9993b6eed7ad33a
Pavel Neverov <140485683+3GonoverskyiPoSkidkeAndMalboroRed@users.noreply.github.com>
2025-05-04 12:17:44 +0300

    изменена странциа регистарции

--- views/site/register.php
+++ views/site/register.php
@@ -7,9 +7,40 @@ use yii\bootstrap5\ActiveForm;
 /** @var app\models\RegisterForm $model */
 /** @var ActiveForm $form */
 ?>
+<style>
+body {
+    font-family: 'Helvetica', sans-serif; /* Устанавливаем шрифт Helvetica для всего тела страницы */
+}
+
+.site-register {
+    max-width: 400px;
+    margin: auto;
+    padding: 20px;
+    border-radius: 5px;
+}
+
+.site-register h3 {
+    text-align: center;
+    margin-bottom: 20px;
+    font-family: 'Impact', sans-serif; /* Устанавливаем шрифт Impact для заголовка */
+}
+
+.site-register .form-group {
+    margin-bottom: 15px;
+}
+
+.site-register .btn {
+    width: 100%;
+}
+</style>
+
 <div class="site-register">
 
     <h3>Регистрация</h3>
+
+    <p class="text-center">
+        <?= Html::a('Вход, если уже есть аккаунт', ['/site/login'], ['class' => 'link-primary']) ?>
+    </p>
 
     <?php $form = ActiveForm::begin(); ?>
 
@@ -17,7 +48,7 @@ use yii\bootstrap5\ActiveForm;
         <?= $form->field($model, 'password')->passwordInput() ?>
         <?= $form->field($model, 'full_name') ?>
         <?= $form->field($model, 'phone')->widget(\yii\widgets\MaskedInput::class, [
-    'mask' => '+7(999)-999-99-99',]) ?>
+            'mask' => '+7(999)-999-99-99',]) ?>
         <?= $form->field($model, 'email') ?>
     
         <div class="form-group">
